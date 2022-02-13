382a190a770e5d0c02689316f3cfbe88d34b8880
adam-adair <aar2222@gmail.com>
2022-02-13 17:05:25 -0500

    first broken

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+import PagerView from 'react-native-pager-view';
 
-export default function App() {
+// const Tab = createMaterialBottomTabNavigator();
+
+const Tab = createMaterialTopTabNavigator();
+
+function Screen() {
+  const [msg, setMsg] = React.useState('');
+  const inputRef = React.useRef<TextInput>(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home Screen</Text>
+      <TextInput
+        placeholder='text'
+        multiline
+        value={msg}
+        onChangeText={setMsg}
+        style={{borderWidth: 1, width: 100}}
+        ref={inputRef}
+        onFocus={()=>console.log("focus")}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <PagerView style={{flex: 1}} keyboardDismissMode="none">
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Screen1' component={Screen} />
+        <Tab.Screen name='Screen2' component={Screen} />
+        <Tab.Screen name='Screen3' component={Screen} />
+        <Tab.Screen name='Screen4' component={Screen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </PagerView>
+  );
+}
